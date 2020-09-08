c325df9ae022615f0be9c745bca4d4b9a9b2e277
David Schnurr <schnurr@gmail.com>
2020-09-07 20:59:21 -0700

    fix exports and decoding

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+};
